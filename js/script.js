0e24aa016e47d74ac3cd5482eecd21f15c590c9e
Artem <artem.fullstack@gmail.com>
2022-06-06 18:08:56 +0300

    Test First commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+let number = 5;
+const leftBorderWith = 1;
+
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 12
+};
+
+obj.a = 14;
+
+console.log(obj);
+
+
+{
+    let name = 'Ivan';
+}
+
+console.log(name);
+
+a = 15;
+console.log(a);
